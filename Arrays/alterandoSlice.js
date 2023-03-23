a87164f1e7e424d879939bdeb691e7aa47cbeb25
jose arilson <arilson_dias@hotmail.com>
2023-03-23 16:32:42 -0300

    teste array

---
+++ Arrays/alterandoSlice.js
@@ -0,0 +1,15 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+nomes.splice(1, 1,"carla",1,3,"guga")
+console.log(nomes)
+
+animaisDoAquario = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+animaisDoAquario.splice(1, 0, 'paulo')
+animaisDoAquario.splice(3, 2, 'paula')
+console.log(animaisDoAquario)
+
+/*
+nomes.push("rodrigo")
+
+console.log(nomes)
+*/
---
+++ Arrays/concatenarArray.js
@@ -0,0 +1,7 @@
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+
+const salaNova = salaJS.concat(salaPython)
+
+console.log(salaNova);
